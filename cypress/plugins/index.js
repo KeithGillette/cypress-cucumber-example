e52858fe6c455e22bb1f499fcb9ab86671e82869
Keith Gillette <Keith.Gillette@TaskTrain.co>
2018-06-22 16:44:28 -0400

    Update `cypress/plugins/index.js` with `cypress-cucumber-preprocessor` & `add-typescript-to-cypress` preprocessors

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,13 +8,15 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
 const cucumber = require('cypress-cucumber-preprocessor').default;
 const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
 
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
 module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
   on('file:preprocessor', cypressTypeScriptPreprocessor);
-  on('file:preprocessor', cucumber());
+  // on('file:preprocessor', cucumber());
 };
